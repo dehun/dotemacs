599671fdd4d25c56240bc7726a181c0fc69fde05
Yuriy Netesov <973520+dehun@users.noreply.github.com>
2014-06-27 06:04:50 -0400

    added notifications system and erc support

---
+++ de-notifications.el
@@ -0,0 +1,23 @@
+(require 'slime)
+
+(defvar de-notifications-enabled 1 "notifications switcher")
+
+(defun de-notify-init ()
+  (slime-connect "127.0.0.1" 4005))
+
+(defun de-enable-notifications ()
+  (setq de-notifications-enabled 1))
+
+(defun de-disable-notifications ()
+  (setq de-notifications-enabled 0))
+
+(defun de-notify (origin message)
+  (when (eq de-notifications-enabled 1)
+    (slime-eval `(stumpwm:message 
+                  (common-lisp:concatenate 'common-lisp:string "[" ,origin "] :: " ,message)))))
+
+(de-notify-init)
+(de-enable-notifications)
+(de-notify "emacs" "notifications system started up")
+
+(provide 'de-notifications)
